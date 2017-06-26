a9c8745af77b13e4f8be66de9f19b484389bf392
Cristina Lester <cristina.deelester@gmail.com>
2017-06-26 00:01:58 -0700

    Added slide in animations on scroll

--- js/main.js
+++ js/main.js
@@ -1,14 +1,30 @@
 $(document).ready(function (){
-
   /* smooth scrolling for scroll to top */
   $('.scroll-top').click(function(){
-    $('body,html').animate({scrollTop:0},1000);
+    $('body,html').animate({scrollTop:0},1500);
   })
 
-  /* smooth scrolling for nav sections */
-  $('#nav .navbar-nav li>a').click(function(){
-    var link = $(this).attr('href');
-    var posi = $(link).offset().top+20;
-    $('body,html').animate({scrollTop:posi},700);
-  })
+  /*animation to make the elements to slide in */
+  $(window).scroll(function() {
+    $(".slide-topdown").each(function(){
+      /*get position of the object with class .slide-topdown*/
+      var pos = $(this).offset().top;
+      /*get current vertical position of the scroll bar*/
+      var winTop = $(window).scrollTop();
+      if (pos < winTop + 600) {
+        $(this).addClass("slide-td");
+      }
+    });
+
+    $(".slide-rightleft").each(function(){
+      /*get position of the object with class .slide-rightleft*/
+      var pos = $(this).offset().top;
+      /*get current vertical position of the scroll ba */
+      var winTop = $(window).scrollTop();
+      if (pos < winTop + 600) {
+        $(this).addClass("slide-rl");
+      }
+    });
+  });
+
 });
